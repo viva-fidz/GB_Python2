e5ecb8e0d35654502779a35ef0347ba388a8766e
viva-fidz <viva.fidz@yandex.ru>
2017-04-16 19:02:40 +0300

    Update need_hashes_my.py

--- lesson_1/homework/need_hashes_my.py
+++ lesson_1/homework/need_hashes_my.py
@@ -2,7 +2,7 @@ import hashlib
 
 
 def hesh_sum(line, algorithm):
-    '''вычисление заданной хэш-суммы для строки
+    '''вычисление заданной хэш-суммы для строки 
     '''
     if algorithm == 'md5':
         h = hashlib.md5()
@@ -10,15 +10,21 @@ def hesh_sum(line, algorithm):
         h = hashlib.sha512()
     elif algorithm == 'sha1':
         h = hashlib.sha1()
-
     line = line.encode('koi8-r')
     h.update(line)
     return h.hexdigest()
 
 
-with open('need_hashes.csv') as f:
+with open('need_hashes.csv', 'rb') as f:
     res = ''
     for line in f:
-        hline = line.split(';')
-        res += hesh_sum(hline[0], hline[1]) + '\n'
+       hline = line.decode().split(';')
+       hashed_line = '{}; {}; {}; {}'.format(
+                   hline[0], hline[1], hesh_sum(hline[0], hline[1]), '\n')
+       res += hashed_line
+
+
+with open('need_hashes.csv', 'w') as f:
     f.write(res)
+
+
