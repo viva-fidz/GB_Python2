218a9f7c6cf49d69eb82217bc725dc2a16f70525
F <you@example.com>
2017-04-16 03:30:12 +0300

    without test

---
+++ lesson_1/homework/need_hashes_my.py
@@ -0,0 +1,26 @@
+import hashlib
+
+
+def hesh_sum(line, algorithm):
+    '''вычисление заданной хэш-суммы для строки
+    '''
+    if algorithm == 'md5':
+        h = hashlib.md5()
+    elif algorithm == 'sha512':
+        h = hashlib.sha512()
+    elif algorithm == 'sha1':
+        h = hashlib.sha1()
+
+    line = line.encode('koi8-r')
+    h.update(line)
+    return h.hexdigest()
+
+
+with open('need_hashes.csv') as f:
+    res = ''
+    for line in f:
+        hline = line.split(';')
+        hashed_line = '{}; {}; {}; {}'.format(
+            hline[0], hline[1], hesh_sum(hline[0], hline[1]), '\n')
+        res += hashed_line
+    f.write(res)
