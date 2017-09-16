50e1b0d0aee54cabd7ce9bb9d77f2e6e95f716a8
viva-fidz <viva.fidz@yandex.ru>
2017-09-16 23:29:29 +0300

    pep8

--- lesson_1/homework/need_hashes_my.py
+++ lesson_1/homework/need_hashes_my.py
@@ -1,30 +1,29 @@
 import hashlib
 
 
-def hesh_sum(line, algorithm):
-    '''вычисление заданной хэш-суммы для строки 
-    '''
+def hash_sum(line, algorithm):
+    """вычисление заданной хэш-суммы для строки
+    """
     if algorithm == 'md5':
         h = hashlib.md5()
     elif algorithm == 'sha512':
         h = hashlib.sha512()
     elif algorithm == 'sha1':
         h = hashlib.sha1()
-    line = line.encode('koi8-r')
+    line = line.encode()
     h.update(line)
     return h.hexdigest()
 
 
-with open('need_hashes.csv', 'rb') as f:
-    res = ''
-    for line in f:
-       hline = line.decode().split(';')
-       hashed_line = '{}; {}; {}; {}'.format(
-                   hline[0], hline[1], hesh_sum(hline[0], hline[1]), '\n')
-       res += hashed_line
+if __name__ == '__main__':
 
+    with open('need_hashes.csv', 'rb') as f:
+        res = ''
+        for line in f:
+            hline = line.decode().split(';')
+            hashed_line = '{}; {}; {}; {}'.format(
+                hline[0], hline[1], hash_sum(hline[0], hline[1]), '\n')
+            res += hashed_line
 
-with open('need_hashes.csv', 'w') as f:
-    f.write(res)
-
-
+    with open('need_hashes.csv', 'w') as f:
+        f.write(res)
